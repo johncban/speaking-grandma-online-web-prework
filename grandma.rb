ad290edc126f60c6a5eeaf72c07edc577f0b1734
johncban <johncban@gmail.com>
2019-04-12 18:24:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,6 @@
 def speak_to_grandma(talk)
   if talk == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif talk == talk.capitalize
-    return "HUH?! SPEAK UP, SONNY!"
   elsif talk == talk.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif talk == talk.reverse
