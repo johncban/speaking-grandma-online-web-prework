1722622276325abdbb462ba57417b3510f8cd1f5
johncban <johncban@gmail.com>
2019-04-12 18:26:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(talk)
   if talk == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif talk == talk.upcase
+  elsif talk == talk.swapcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif talk == talk.reverse
     return "HUH?! SPEAK UP, SONNY!"
